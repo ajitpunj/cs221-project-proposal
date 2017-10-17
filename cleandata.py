cd381d3e23aa9d2107e601b8cc34f99786d9c527
Ajit Punj <ajitpunj@ajits-mbp.lan>
2017-10-16 19:24:48 -0700

    cleaned up script

--- cleandata.py
+++ cleandata.py
@@ -11,9 +11,8 @@ monthCol = 0
 
 for file in CSVfiles:
 	filename = file.split('/')
-	filename1 = 'refined_data_'+filename[-1]
-	#TODO fix naming and clean up
-	writer = csv.writer(open(filename1, 'wb'))
+	filename = 'refined_data_'+filename[-1]
+	writer = csv.writer(open(filename, 'wb'))
 	
 	with open(file, 'rb') as csvfile:
 		currentYear = csv.reader(csvfile, delimiter=',')
@@ -27,5 +26,6 @@ for file in CSVfiles:
 				monthCol = i
 
 		for row in currentYear:
+			#check if the month matches, then print to new csv file
 			if row[monthCol] == str(requiredMonth):
 				writer.writerow(row)
