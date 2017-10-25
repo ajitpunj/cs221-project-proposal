5d94f38aee24c3d2215c97407931db2809b20d73
kdurfee <kpdurfee@standford.edu>
2017-10-25 15:55:31 -0700

    refined data for 2008 so we dont have to download it when we try proof of concept stuff. Added the TA who recommended the project to the ca.txt. Also modifed baseline to guess that things are 3x as likely near xmas but the results were actually worse than random

--- baseline.py
+++ baseline.py
@@ -10,46 +10,53 @@ dayCol = 2
 depDelayCol = 15
 cancelledCol = 21
 
-cancelProb = .0218
-delayProb = .217 
+setCancelProb = .0218
+setDelayProb = .217 
 
 avgBaselineDelay = 57
 
 iterationVal = 1000
 
 for file in CSVfiles:
-	totalCancel = 0
-	totalDelay = 0
-	for i in range(iterationVal):
-		with open(file, 'rb') as csvfile:
-			delayPredictError = 0
-			correctCancelPredict = 0
-			totalRows = 0
+    totalCancel = 0
+    totalDelay = 0
+    for i in range(iterationVal):
+        with open(file, 'rb') as csvfile:
+            delayPredictError = 0
+            correctCancelPredict = 0
+            totalRows = 0
 
-			currentYear = csv.reader(csvfile, delimiter=',')
+            currentYear = csv.reader(csvfile, delimiter=',')
 
-			for row in currentYear:
-				#get cancellation and delay data from csv
-				realCancelled = int(row[cancelledCol])
-				realDelay = 0
-				baselineCancel = 0
-				baselineDelay = 0
-				#only get realDelay if cancelled column is 0, if it is 1 then delay will be NA
-				if realCancelled == 0:
-					realDelay = int(row[depDelayCol])
-
-				#predict cancel with certain probability, else predict a delay of 57 min
-				if random.random() < cancelProb:
-					baselineCancel = 1
-				elif random.random() < delayProb:
-					baselineDelay = avgBaselineDelay
+            for row in currentYear:
+                #get cancellation and delay data from csv
+                realCancelled = int(row[cancelledCol])
+                realDelay = 0
+                baselineCancel = 0
+                baselineDelay = 0
+                #only get realDelay if cancelled column is 0, if it is 1 then delay will be NA
+                if realCancelled == 0:
+                    realDelay = int(row[depDelayCol])
+                    
+                #predict cancel with certain probability, else predict a delay of 57 min
+                #if we are within the week of chrimstas, multiply the probabilities by 3
+                if (int(row[dayCol]) >= 18 or int(row[dayCol])<=31):
+                    cancelProb = 3*setCancelProb
+                    delayProb = 3*setDelayProb
+                else:
+                    cancelProb = setCancelProb
+                    delayProb = setDelayProb                    
+                if random.random() < cancelProb:
+                    baselineCancel = 1
+                elif random.random() < delayProb:
+                    baselineDelay = avgBaselineDelay
 				#check if actual matches baseline predictions:
-				if baselineCancel == realCancelled:
-					correctCancelPredict += 1
-				if baselineDelay != realDelay:
-					delayPredictError += (abs(baselineDelay-realDelay))
-				totalRows += 1
-		totalCancel += correctCancelPredict/totalRows
-		totalDelay += float(delayPredictError)/float(totalRows)
-		print file, 'iteration ',i,' delay error: ', float(delayPredictError)/float(totalRows), ' correct cancellation prediction: ', correctCancelPredict/totalRows
-	print 'avg over all runs: delay - ', totalDelay/iterationVal, 'cancel - ',totalCancel/iterationVal
+                if baselineCancel == realCancelled:
+                    correctCancelPredict += 1
+                if baselineDelay != realDelay:
+                    delayPredictError += (abs(baselineDelay-realDelay))
+                totalRows += 1
+        totalCancel += correctCancelPredict/totalRows
+        totalDelay += float(delayPredictError)/float(totalRows)
+        print file, 'iteration ',i,' delay error: ', float(delayPredictError)/float(totalRows), ' correct cancellation prediction: ', correctCancelPredict/totalRows
+    print 'avg over all runs: delay - ', totalDelay/iterationVal, 'cancel - ',totalCancel/iterationVal
